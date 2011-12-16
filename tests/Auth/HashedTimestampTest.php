44d7063266568946218a48f06122698e25dd1c3e
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-16 21:25:44 +0900

    Refactoring: Reduced test code duplication.

--- tests/Auth/HashedTimestampTest.php
+++ tests/Auth/HashedTimestampTest.php
@@ -23,11 +23,7 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
      */
     public function auth_should_be_true_if_timestamp_is_not_expired_and_hash_is_correct()
     {
-        $authenticator = new Auth_HashedTimestamp(
-            $this->_hashGenerator,
-            self::EXPIRATION_SECONDS,
-            $this->_currentTimestampProvider
-        );
+        $authenticator = $this->createAuthenticator();
         $now  = self::CURRENT_TIMESTAMP;
         $hash = call_user_func($this->_hashGenerator, $now);
         $this->assertTrue($authenticator->auth($hash, $now));
@@ -38,11 +34,7 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
      */
     public function auth_should_be_true_if_timestamp_is_just_expiration_time()
     {
-        $authenticator = new Auth_HashedTimestamp(
-            $this->_hashGenerator,
-            self::EXPIRATION_SECONDS,
-            $this->_currentTimestampProvider
-        );
+        $authenticator = $this->createAuthenticator();
         $now  = self::CURRENT_TIMESTAMP + self::EXPIRATION_SECONDS;
         $hash = call_user_func($this->_hashGenerator, $now);
         $this->assertTrue($authenticator->auth($hash, $now));
@@ -53,11 +45,7 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
      */
     public function auth_should_be_false_if_timestamp_is_expired()
     {
-        $authenticator = new Auth_HashedTimestamp(
-            $this->_hashGenerator,
-            self::EXPIRATION_SECONDS,
-            $this->_currentTimestampProvider
-        );
+        $authenticator = $this->createAuthenticator();
         $now  = self::CURRENT_TIMESTAMP + self::EXPIRATION_SECONDS + 1; // expired!
         $hash = call_user_func($this->_hashGenerator, $now);
         $this->assertFalse($authenticator->auth($hash, $now));
@@ -68,11 +56,7 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
      */
     public function auth_should_be_false_if_hash_is_incorrect()
     {
-        $authenticator = new Auth_HashedTimestamp(
-            $this->_hashGenerator,
-            self::EXPIRATION_SECONDS,
-            $this->_currentTimestampProvider
-        );
+        $authenticator = $this->createAuthenticator();
         $now  = self::CURRENT_TIMESTAMP;
         $hash = 'Incorrect Hash';
         $this->assertFalse($authenticator->auth($hash, $now));
@@ -83,12 +67,17 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
      */
     public function generateHash_should_be_hash_generated_by_hash_generator()
     {
-        $authenticator = new Auth_HashedTimestamp(
-            $this->_hashGenerator,
-            self::EXPIRATION_SECONDS,
-            self::CURRENT_TIMESTAMP
-        );
+        $authenticator = $this->createAuthenticator();
         $timestamp = time();
         $this->assertEquals((string)$timestamp, $authenticator->generateHash($timestamp));
+    }
+
+    protected function createAuthenticator()
+    {
+        return new Auth_HashedTimestamp(
+            $this->_hashGenerator,
+            self::EXPIRATION_SECONDS,
+            $this->_currentTimestampProvider
+        );
     }
 }
