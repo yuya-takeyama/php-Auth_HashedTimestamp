25034310bb59e176b4c52b2d78eeba7f21ce83c8
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-16 20:37:04 +0900

    Add: Test.

--- tests/Auth/HashedTimestampTest.php
+++ tests/Auth/HashedTimestampTest.php
@@ -4,6 +4,7 @@ require_once dirname(__FILE__) . '/../../src/Auth/HashedTimestamp.php';
 class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
 {
     const CRITERIA_TIMESTAMP = 1324034000;
+    const EXPIRATION_SECONDS = 60;
 
     public function setUp()
     {
@@ -23,6 +24,21 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
             self::CRITERIA_TIMESTAMP
         );
         $now  = self::CRITERIA_TIMESTAMP;
+        $hash = call_user_func($this->_hashGenerator, $now);
+        $this->assertTrue($authenticator->auth($now, $hash));
+    }
+
+    /**
+     * @test
+     */
+    public function auth_should_be_false_if_timestamp_is_just_expiration_time()
+    {
+        $authenticator = new Auth_HashedTimestamp(
+            $this->_hashGenerator,
+            60,
+            self::CRITERIA_TIMESTAMP
+        );
+        $now  = self::CRITERIA_TIMESTAMP + self::EXPIRATION_SECONDS;
         $hash = call_user_func($this->_hashGenerator, $now);
         $this->assertTrue($authenticator->auth($now, $hash));
     }
