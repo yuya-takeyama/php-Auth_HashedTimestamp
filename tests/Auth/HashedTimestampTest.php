e1aa476708723e14841d270d311b689d35a6b0c7
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-16 21:29:26 +0900

    Fix: Indentation.

--- tests/Auth/HashedTimestampTest.php
+++ tests/Auth/HashedTimestampTest.php
@@ -24,8 +24,8 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
     public function auth_should_be_true_if_timestamp_is_not_expired_and_hash_is_correct()
     {
         $authenticator = $this->createAuthenticator();
-        $timestamp  = self::CURRENT_TIMESTAMP;
-        $hash = call_user_func($this->_hashGenerator, $timestamp);
+        $timestamp     = self::CURRENT_TIMESTAMP;
+        $hash          = call_user_func($this->_hashGenerator, $timestamp);
         $this->assertTrue($authenticator->auth($hash, $timestamp));
     }
 
@@ -35,8 +35,8 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
     public function auth_should_be_true_if_timestamp_is_just_expiration_time()
     {
         $authenticator = $this->createAuthenticator();
-        $timestamp  = self::CURRENT_TIMESTAMP + self::EXPIRATION_SECONDS;
-        $hash = call_user_func($this->_hashGenerator, $timestamp);
+        $timestamp     = self::CURRENT_TIMESTAMP + self::EXPIRATION_SECONDS;
+        $hash          = call_user_func($this->_hashGenerator, $timestamp);
         $this->assertTrue($authenticator->auth($hash, $timestamp));
     }
 
@@ -46,8 +46,8 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
     public function auth_should_be_false_if_timestamp_is_expired()
     {
         $authenticator = $this->createAuthenticator();
-        $timestamp  = self::CURRENT_TIMESTAMP + self::EXPIRATION_SECONDS + 1; // expired!
-        $hash = call_user_func($this->_hashGenerator, $timestamp);
+        $timestamp     = self::CURRENT_TIMESTAMP + self::EXPIRATION_SECONDS + 1; // expired!
+        $hash          = call_user_func($this->_hashGenerator, $timestamp);
         $this->assertFalse($authenticator->auth($hash, $timestamp));
     }
 
@@ -57,8 +57,8 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
     public function auth_should_be_false_if_hash_is_incorrect()
     {
         $authenticator = $this->createAuthenticator();
-        $timestamp  = self::CURRENT_TIMESTAMP;
-        $hash = 'Incorrect Hash';
+        $timestamp     = self::CURRENT_TIMESTAMP;
+        $hash          = 'Incorrect Hash';
         $this->assertFalse($authenticator->auth($hash, $timestamp));
     }
 
@@ -68,7 +68,7 @@ class Auth_HashedTimestampTest extends PHPUnit_Framework_TestCase
     public function generateHash_should_be_hash_generated_by_hash_generator()
     {
         $authenticator = $this->createAuthenticator();
-        $timestamp = time();
+        $timestamp     = time();
         $this->assertEquals(
             call_user_func($this->_hashGenerator, $timestamp),
             $authenticator->generateHash($timestamp)
